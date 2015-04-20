53da861be999ffc331e38fedb107eed682276c3f
Ryan Coon <rybread354@aol.com>
2015-04-19 23:36:12 -0500

    Code from next video lesson

---
+++ arithmetic_operators.py
@@ -0,0 +1,21 @@
+a = 4 + 5
+print(a)
+
+f = 5.5 + 10
+print(f)
+
+# division and floor division
+b = f / a
+print(b)
+b = f // a
+print(b)
+
+c = 5 / 3
+print(c)
+print(round(c))
+
+r = 5 % 3
+print(r)
+
+e = 5**2
+print(e)
